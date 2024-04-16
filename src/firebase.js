06f5ea802faf50b07019e7100bf0a84048184d91
Angelo//Telamo <126788602+AngeloTelamo@users.noreply.github.com>
2024-04-16 08:27:44 +0800

    database changes

--- src/firebase.js
+++ src/firebase.js
@@ -3,24 +3,15 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from '@firebase/firestore';
 import { getAuth } from "firebase/auth";
 
-// const firebaseConfig = {
-//   apiKey: "AIzaSyBm2_-Z8m3s-5EhmqiJ1bjNwcw4R40vj4Q",
-//   authDomain: "chatbot-database-cb6fe.firebaseapp.com",
-//   projectId: "chatbot-database-cb6fe",
-//   storageBucket: "chatbot-database-cb6fe.appspot.com",
-//   messagingSenderId: "942949212643",
-//   appId: "1:942949212643:web:226772e4f7cb91bc7300d6",
-//   measurementId: "G-L651XGLJG2"
-// };
-
+// Your web app's Firebase configuration
+//personal dev databse 
 const firebaseConfig = {
-  apiKey: "AIzaSyDmqGRl1yXVd5-UbVbSK3GqHFsExOgCTso",
-  authDomain: "chatbot-954b3.firebaseapp.com",
-  projectId: "chatbot-954b3",
-  storageBucket: "chatbot-954b3.appspot.com",
-  messagingSenderId: "984216850141",
-  appId: "1:984216850141:web:e980e5c93fb538dc30d534",
-  measurementId: "G-GRQLP2V1B2"
+  apiKey: "AIzaSyBl-vUwQ4tnXIX3En_UCOCxEOfh5GggpUE",
+  authDomain: "chatbot-3179e.firebaseapp.com",
+  projectId: "chatbot-3179e",
+  storageBucket: "chatbot-3179e.appspot.com",
+  messagingSenderId: "897109517535",
+  appId: "1:897109517535:web:22804e7bd7280fa3b8f56a"
 };
 
 // Initialize Firebase app
